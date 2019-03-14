afd2e1a4b1725e3db81d08f1c4b12cc493e4fb4b
DRIFTKINGDK <thulasitharan.gt96@gmail.com>
2019-03-14 08:42:06 +0530

    fnction update

--- src/main/scala/com/vsms/service/ProjectFunction.scala
+++ src/main/scala/com/vsms/service/ProjectFunction.scala
@@ -5,40 +5,36 @@ import org.apache.spark.sql.DataFrame
 
 package object ProjectFunction {
 
-    def Retrival_info_fun(scn_no: Int ,Query_data: DataFrame, ArgMap : collection.mutable.Map[String,String]) : DataFrame =
-  {
+    def Retrival_info_fun(scn_no: Int ,Query_data: DataFrame, ArgMap : collection.mutable.Map[String,String]) : DataFrame = {
 
-    val Error_df= spark.createDataFrame(Seq(("Empty_DF","Invalid SCN Number"))).toDF("Error","ERROR_MESSAGE")
+      var Scn_df:DataFrame=null
+      val SqlString:String =try {Query_data.filter("SCN_NO=" + scn_no).select("QUERY").rdd.map(r => r(0)).collect().mkString(",")}
+      println(SqlString)
+      var SqlString_final:String = null
 
-     if (scn_no ==1)
-    {
-      val SqlString=Query_data.filter("SCN_NO="+scn_no).select("QUERY").rdd.map(r=>r(0)).collect().mkString(",")
-      println(SqlString)
-      var SqlString_final = SqlString.replace("%t1%",ArgMap("%t1%").toString).replace("%where_condition%",ArgMap("%where_condition%").toString).replace("*",ArgMap("*").toString)
-      println(SqlString_final)
-      var scn_2_df=spark.sql(SqlString_final.toString())
-      return scn_2_df
+      if (scn_no == 1) {
+        SqlString_final = SqlString.replace("%t1%", ArgMap("%t1%").toString).replace("%where_condition%", ArgMap("%where_condition%").toString).replace("*", ArgMap("*").toString)
+        println(SqlString_final)
+        Scn_df = spark.sql(SqlString_final.toString())
+        Scn_df
+      }
+      else if (scn_no == 2) {
+        SqlString_final = SqlString.replace("%t1%", ArgMap("%t1%").toString).replace("%t2%", ArgMap("%t2%").toString).replace("%join%", ArgMap("%join%").toString).replace("%join_condition%", ArgMap("%join_condition%").toString).replace("*", ArgMap("*").toString)
+        println(SqlString_final)
+        Scn_df = spark.sql(SqlString_final.toString())
+        Scn_df
+      }
+      else if (scn_no == 3) {
+        SqlString_final = SqlString.replace("*", ArgMap("*").toString).replace("%t1%", ArgMap("%t1%").toString).replace("%t2%", ArgMap("%t2%").toString).replace("%join%", ArgMap("%join%").toString).replace("%join_condition%", ArgMap("%join_condition%").toString).replace("%where_condition%", ArgMap("%where_condition%").toString)
+        println(SqlString_final)
+        Scn_df = spark.sql(SqlString_final.toString())
+        Scn_df
+      }
+      else
+      {
+        Scn_df= spark.createDataFrame(Seq(("Empty_DF", "Invalid SCN Number"))).toDF("Error", "ERROR_MESSAGE")
+        Scn_df
+      }
     }
-    else if (scn_no ==2)
-    {
-      val SqlString=Query_data.filter("SCN_NO="+scn_no).select("QUERY").rdd.map(r=>r(0)).collect().mkString(",")
-      println(SqlString)
-      var SqlString_final = SqlString.replace("%t1%",ArgMap("%t1%").toString).replace("%t2%",ArgMap("%t2%").toString).replace("%join%",ArgMap("%join%").toString).replace("%join_condition%",ArgMap("%join_condition%").toString).replace("*",ArgMap("*").toString)
-      println(SqlString_final)
-      var scn_3_df=spark.sql(SqlString_final.toString())
-      return scn_3_df
-    }
-        else if (scn_no==3)
-    {
-      val SqlString=Query_data.filter("SCN_NO="+scn_no).select("QUERY").rdd.map(r=>r(0)).collect().mkString(",")
-      println(SqlString)
-      var SqlString_final = SqlString.replace("*",ArgMap("*").toString).replace("%t1%",ArgMap("%t1%").toString).replace("%t2%",ArgMap("%t2%").toString).replace("%join%",ArgMap("%join%").toString).replace("%join_condition%",ArgMap("%join_condition%").toString).replace("%where_condition%",ArgMap("%where_condition%").toString)
-      println(SqlString_final)
-      var scn_4_df=spark.sql(SqlString_final.toString())
-      return scn_4_df
-    }
-
-    Error_df
-  }
 
 }
--- src/main/scala/com/vsms/service/ProjectFunctionDfVersion.scala
+++ src/main/scala/com/vsms/service/ProjectFunctionDfVersion.scala
@@ -15,31 +15,36 @@ object ProjectFunctionDfVersion
 
   {
 //creating Error DataFrame
-    val Error_df= spark.createDataFrame(Seq(("Empty_DF","Invalid SCN Number"))).toDF("Error","ERROR_MESSAGE")
+    var Scn_df: DataFrame = null
 
     // SCN 1 single data frame with where and select conditions
     if (scn_no ==1)
     {
-      var DF_final = DFMap("%df1%").filter(ArgMap("%where_condition%").toString).select(ArgMap("*").toString)
-      return DF_final
+      Scn_df = DFMap("%df1%").filter(ArgMap("%where_condition%").toString).select(ArgMap("*").toString)
+      Scn_df
     }
     // SCN 2 , 2 data frame with Join , Join type , aliases , multiple column in join ,where and select conditions
 
     else if (scn_no ==2)
     {
     //val DF_final= DFMap("df1").as("a").drop(ArgMap("%drop_coloumn_a%").toString).join(DFMap("df2").as("b").drop(ArgMap("%drop_coloumn_b%").toString),ArgMap("%join_condition%").toString).select(ArgMap("*").toString).filter(ArgMap("%where_condition%").toString)
-    val DF_final= (DFMap("%df1%").drop(ArgMap("%drop_a_condition%"))).as("a").join((DFMap("%df2%").drop(ArgMap("%drop_b_condition%"))).as("b"),List_map("%join_condition%"),ArgMap("%join_type%").toString).select(ArgMap("*").toString).filter(ArgMap("%where_condition%").toString)
-    return DF_final
+      Scn_df= (DFMap("%df1%").drop(ArgMap("%drop_a_condition%"))).as("a").join((DFMap("%df2%").drop(ArgMap("%drop_b_condition%"))).as("b"),List_map("%join_condition%"),ArgMap("%join_type%").toString).select(ArgMap("*").toString).filter(ArgMap("%where_condition%").toString)
+      Scn_df
     }
 
     // SCN 3 , 2 data frame with Join , aliases , multiple column in join ,where and select conditions
 
     else if (scn_no ==3)
     {
-      val DF_final= (DFMap("%df1%").drop(ArgMap("%drop_a_condition%")).as("a")).join((DFMap("%df2%").drop(ArgMap("%drop_b_condition%"))).as("b"),List_map("%join_condition%")).select(ArgMap("*").toString).filter(ArgMap("%where_condition%").toString)
-      return DF_final
+      Scn_df= (DFMap("%df1%").drop(ArgMap("%drop_a_condition%")).as("a")).join((DFMap("%df2%").drop(ArgMap("%drop_b_condition%"))).as("b"),List_map("%join_condition%")).select(ArgMap("*").toString).filter(ArgMap("%where_condition%").toString)
+      Scn_df
     }
-    Error_df
+    else
+    {
+      Scn_df= spark.createDataFrame(Seq(("Empty_DF","Invalid SCN Number"))).toDF("Error","ERROR_MESSAGE")
+      Scn_df
+    }
+
   }
 
 
